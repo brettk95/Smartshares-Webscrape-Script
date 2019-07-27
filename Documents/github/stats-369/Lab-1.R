fda6589ac1fc7f7c2b3a0506e58cfae93dd5d835
Brett <brettkim123@gmail,com>
2019-07-27 14:08:11 +1200

    27th July update, think I've solved Q1. Working on Q2

--- Documents/github/stats-369/Lab-1.R
+++ Documents/github/stats-369/Lab-1.R
@@ -1,17 +1,36 @@
 library("tidyverse")
 log_tib = read_csv("events_log.csv", col_types = 'cccccicii')
-log_tib_sep = log_tib %>% separate(timestamp, c('Day', 'Time'), sep = 8)
+log_tib = log_tib %>% separate(timestamp, c('Day', 'Time'), sep = 8)
 
 # Question 1a
  
-num_visitPage = log_tib_sep %>% group_by(session_id) %>% filter(action == 'visitPage') %>% group_by(Day) %>% count()
-num_notvisitPage = log_tib_sep %>% group_by(session_id) %>% filter(action != 'visitPage') %>% group_by(Day) %>% count()
-num_notvisitPage = num_notvisitPage[-1,]
-
-num_visitPage$Proportion = num_visitPage$n/num_notvisitPage$n
+click_through_df = 
+  log_tib %>% 
+  group_by(Day, session_id) %>% 
+  summarise(visited = sum(action == 'visitPage'), 
+            not_visited = sum(action != 'visitPage'), 
+            ratio = visited/not_visited) %>% 
+  group_by(Day) %>% 
+  summarise(visited = sum(visited), 
+            not_visited = sum(not_visited), 
+            ratio = visited/not_visited)
 
 # Question 1b
-num_vp_group = log_tib_sep %>% group_by(session_id) %>% filter(action == 'visitPage') %>% group_by(Day,group) %>% count()
-num_nvp_group = log_tib_sep %>% group_by(session_id) %>% filter(action != 'visitPage') %>% group_by(Day,group) %>% count()
-num_nvp_group = num_nvp_group[-1,]
+click_through_group_df = 
+  log_tib %>% 
+  group_by(Day, session_id,group) %>% 
+  summarise(visited = sum(action == 'visitPage'), 
+            not_visited = sum(action != 'visitPage'))%>% 
+  group_by(Day, group) %>%
+  summarise(visited = sum(visited), 
+            not_visited = sum(not_visited), 
+            ratio = visited/not_visited)
 
+# Question 2
+
+log_tib %>% 
+  group_by(session_id) %>% 
+  arrange(desc(Time), .by_group = TRUE) %>% 
+  filter(action == 'visitPage') %>% 
+  group_by(session_id) %>% 
+  summarise(first_page = first(page_id))
